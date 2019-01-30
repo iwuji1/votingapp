b96056181ea5654b7bcd40b4dd32cbb126983c09
Obinna <obinnaops@gmail.com>
2019-01-31 00:32:05 +0100

    user model password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
   validates :name, presence: true
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
   has_many :votes
   accepts_nested_attributes_for :votes
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190129150352) do
+ActiveRecord::Schema.define(version: 20190130231905) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "votes", force: :cascade do |t|
