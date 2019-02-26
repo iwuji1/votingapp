74fd4593f3af7b4063257d9a7b867994a5d51246
Obinna <obinnaops@gmail.com>
2019-02-26 14:30:05 -0600

    hashed belongs_to association

--- app/models/ballot.rb
+++ app/models/ballot.rb
@@ -1,5 +1,5 @@
 class Ballot < ApplicationRecord
-  belongs_to :user
+  #belongs_to :user
   has_many :votes
   accepts_nested_attributes_for :votes
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -24,8 +24,7 @@ end
 Ballot.create(
   ballot_name: "People vote test",
   candidates: "bob box, susan small, merry christmas, why me",
-  rank: "1st, 2nd, 3rd, 4th",
-)
+  rank: "1st, 2nd, 3rd, 4th")
 
 for use in User.all
     6.times do |time|
@@ -33,7 +32,6 @@ for use in User.all
         user_id: use.id,
         candidate_name: [0,1,2,3].sample,
         rank: [0,1,2,3].sample,
-        ballot_id: 0,
-      )
+        ballot_id: 0)
   end
 end
