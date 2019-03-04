b556bc1e49db1b635b433d039ef7e39a17842f52
Obinna <obinnaops@gmail.com>
2019-03-04 16:52:10 -0600

    change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name:  "admin",
-             email: "admin@gmail.com",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             admin: true)
-
 # 20.times do |time|
 #   User.create!(
 #     name: "person#{time}",
