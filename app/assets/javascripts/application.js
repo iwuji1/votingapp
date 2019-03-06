2f9dccd64266319ba0abf5c2901727d6d89c6cea
Obinna <obinnaops@gmail.com>
2019-03-06 13:39:29 -0600

    javascript bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 //= require turbolinks
 //= require_tree .
 
-$(document).ready(function(){
+$(document).on('turbolinks:load',function(){
   $("input[type = 'radio']").change(function(){
     if (this.checked)
     {
