4b7fc08b29954f16c5529d385a7dd8d779385c6a
Obinna <obinnaops@gmail.com>
2019-03-06 12:23:47 -0600

    change array to string

--- db/migrate/20190219143302_create_ballots.rb
+++ db/migrate/20190219143302_create_ballots.rb
@@ -2,8 +2,8 @@ class CreateBallots < ActiveRecord::Migration[5.1]
   def change
     create_table :ballots do |t|
       t.string :ballot_name
-      t.string :candidates, array: true
-      t.string :rank, array: true
+      t.string :candidates
+      t.string :rank
 
       t.timestamps
     end
