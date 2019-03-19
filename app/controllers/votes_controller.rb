c3426e355aecf065a0c125010ac60c03b79b25d8
Hawken Rives <hawkrives@fastmail.fm>
2019-03-18 21:40:03 -0500

    add :index and :new to correct_user and logged_in_user checks

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,6 @@
 class VotesController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :new, :edit, :update, :destroy]
+  before_action :correct_user, only: [:index, :new, :edit, :update]
   before_action :admin_user, only: [:index, :destroy]
 
   def index
