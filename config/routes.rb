30ad31264733e6d87457d5e3d4e68c8be4fa9633
Hawken Rives <hawkrives@fastmail.fm>
2019-03-18 21:39:03 -0500

    remove signup form routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,16 @@
 Rails.application.routes.draw do
-
   get 'sessions/new'
-
   get 'ballots/new'
 
   resources :votes
   resources :front_pages
-  # get 'votes/index'
-  #
-  # get 'votes/new'
-  #
-  # get 'votes/create'
 
   root 'front_pages#home'
 
   get '/help', to: 'front_pages#help'
-  get '/vot', to: 'votes#new'
   get '/vote', to: 'vote#index'
   get '/thanks', to: 'front_pages#thankyou'
   get '/dat', to: 'front_pages#dataform'
-  get '/signup', to:'users#new'
-  post '/signup', to:'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
@@ -38,12 +28,6 @@ Rails.application.routes.draw do
   resources :ballots do
     resources :votes, only: [:create, :new, :index]
   end
-
-  #get 'front_pages/home'
-
-  #get 'front_pages/help'
-
-  #get 'front_pages/vote'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
